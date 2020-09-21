83a9f6261b66ddbb7bef7aa16337a4c4c666a57e
Kryptic Sound <67302177+kryptic-sound@users.noreply.github.com>
2020-09-21 16:37:22 -0400

    rm: modal

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,6 @@ import Header from '../components/layout/Header'
 import Main from '../components/Main'
 import Footer from '../components/layout/Footer'
 
-import TransitionsModal from '../components/EventModal'
-
 class IndexPage extends React.Component {
   constructor(props) {
     super(props)
@@ -97,7 +95,6 @@ class IndexPage extends React.Component {
         <div className={`body ${this.state.loading} ${this.state.isArticleVisible ? 'is-article-visible' : ''}`}>
           <div id="wrapper">
             <Header onOpenArticle={this.handleOpenArticle} timeout={this.state.timeout} />
-            <TransitionsModal />
             <Main
               isArticleVisible={this.state.isArticleVisible}
               timeout={this.state.timeout}
