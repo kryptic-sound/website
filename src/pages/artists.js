710348a6d973ed940dd8076aca50a76d02bb1138
Kryptic Sound <67302177+kryptic-sound@users.noreply.github.com>
2021-01-04 22:47:15 -0500

    refact: artists .. about

---
+++ src/pages/artists.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/layout/Footer'
+
+import Layout from '../components/layout'
+import NavFooter from '../components/layout/NavFooter'
+
+const AboutPage = () => (
+  <Layout>
+    
+    <NavFooter />
+    <Footer />
+  </Layout>
+)
+
+export default AboutPage
